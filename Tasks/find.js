7b3d79c1decd08e464b263636e104532598a815b
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-01-09 23:43:24 +0200

    Update find.js
    Remove unnecessary

--- Tasks/find.js
+++ Tasks/find.js
@@ -4,13 +4,12 @@
 "use strict";
 
 const find = (object, ...rest) => {
-  const value = rest.pop(1);
+  const value = rest.pop();
   for (const name in object) {
     if (object[name] === value) {
       return name;
     }
   }
-  rest.push(5020);
   return undefined;
 };
 
