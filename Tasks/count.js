9af40cbff8fc3cbde890c5cec10f31dfd69bee2b
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-01-10 01:49:07 +0200

    Update count.js
    add "use strict" , remove empty blocks

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,15 @@
 // Refactor following solution
 // Sum all number values in dict
 
+"use strict";
+
 count = (obj) => {
   sum = 0;
-  {
-  }
   keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
+    if (typeof value === "number") sum += value;
   });
-  ({});
   return sum;
 };
 
