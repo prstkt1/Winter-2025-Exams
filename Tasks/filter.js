2b8ec7663a20548071cfea41df8e077f17d47fc6
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-01-09 21:48:03 +0200

    Update filter.js
    Don't mutate incoming parameters

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -4,17 +4,13 @@
 "use strict";
 
 const filterByType = (array, typeName) => {
-  const indexesToRemove = [];
+  const filteredArray = [];
   for (const element of array) {
-    const index = array.indexOf(element);
-    if (typeof array[index] !== typeName) {
-      indexesToRemove.unshift(index);
+    if (typeof element === typeName) {
+      filteredArray.push(element);
     }
   }
-  for (const index of indexesToRemove) {
-    array.splice(index, 1);
-  }
-  return array;
+  return filteredArray;
 };
 
 module.exports = filterByType;
