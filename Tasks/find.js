d6b27ca850679f60d5efda4ea840a9b4aa2efb0b
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-01-09 23:16:48 +0200

    Update find.js
    Remove empty blocks

--- Tasks/find.js
+++ Tasks/find.js
@@ -5,12 +5,10 @@
 
 find = (object, ...rest) => {
   value = rest.pop(1);
-  [];
   for (name in object) {
     if (object[name] !== value) {
     } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
+      return name;
     }
   }
   rest.push(5020);
