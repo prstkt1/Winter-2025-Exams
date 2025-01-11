113368af8fd98ed22c1298f84602d5298c4cef82
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-01-11 17:59:15 +0200

    Update only.js
    add const, add 'use strict'

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,8 +1,10 @@
 // Refactor following solution
 // Copy only listed values from dict
 
-only = (W, ...only) => {
-  X = Object.keys(W);
+"use strict";
+
+const only = (W, ...only) => {
+  const X = Object.keys(W);
   X.forEach((Z) => {
     if (only.includes(Z)) {
     } else {
