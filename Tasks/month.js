1281fd1cbcca1c63bda7143ab5c7cdcc5ba9dbd7
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-01-11 14:32:56 +0200

    Update month.js
    add const

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,23 +1,23 @@
 // Refactor following solution
 // Get month number
 
-Months = [
-  'jan',
-  'feb',
-  'mar',
-  'apr',
-  'may',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'oct',
-  'nov',
-  'dec',
+const Months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
 ];
 
-Month = (s) => {
-  l = Months.length;
+const Month = (s) => {
+  const l = Months.length;
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
